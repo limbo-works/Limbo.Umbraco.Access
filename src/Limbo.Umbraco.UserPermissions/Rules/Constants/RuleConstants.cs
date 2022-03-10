fd3fa5b677f9a56a8266149c1bc11d7d69d564df
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2022-03-10 10:53:09 +0100

    fix: Changed default error category

--- src/Limbo.Umbraco.UserPermissions/Rules/Constants/RuleConstants.cs
+++ src/Limbo.Umbraco.UserPermissions/Rules/Constants/RuleConstants.cs
@@ -2,7 +2,7 @@
     public static class RuleConstants {
         public static readonly string ErrorCategoryKey = "Limbo.Umbraco.UserPermissions_Error_Category";
         public static readonly string ErrorMessageKey = "Limbo.Umbraco.UserPermissions_Error_Message";
-        public static readonly string DefaultErrorCategory = "Access denied";
+        public static readonly string DefaultErrorCategory = "Adgang nægtet";
         public static readonly string DefaultErrorMessage = "Du har ikke rettighed til at udføre denne handling";
     }
 }
