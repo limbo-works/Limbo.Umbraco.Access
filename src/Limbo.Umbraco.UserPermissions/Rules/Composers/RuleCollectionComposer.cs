2cf44519f19c8c12edac68d4c40c70db79bd344b
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2022-06-09 11:43:58 +0200

    fix: Fixes boot error on upgrade (#5)

--- src/Limbo.Umbraco.UserPermissions/Rules/Composers/RuleCollectionComposer.cs
+++ src/Limbo.Umbraco.UserPermissions/Rules/Composers/RuleCollectionComposer.cs
@@ -1,6 +1,8 @@
-﻿using Umbraco.Core.Composing;
+﻿using Umbraco.Core;
+using Umbraco.Core.Composing;
 
 namespace Limbo.Umbraco.UserPermissions.Rules.Composers {
+    [RuntimeLevel(MinLevel = RuntimeLevel.Boot)]
     public class RuleCollectionComposer : IUserComposer {
         public void Compose(Composition composition) {
             composition.Register(typeof(RuleCollection), Lifetime.Singleton);
